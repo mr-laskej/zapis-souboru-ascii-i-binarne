6588ca6e1bf72af96a27a739710c61491855b187
matyas.lasky.020 <matyas.lasky.020@B315-16>
2022-01-31 14:19:11 +0100

    Přidejte soubory projektu.

---
+++ xd2/xd2.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	FILE* vystuptxt = fopen("vystup-txt.txt", "w");
+	FILE* vystupbin = fopen("vystup - bin.txt", "wb");
+
+	int x = 0x1A2B3C4D;
+	fprintf(vystuptxt, "%d", x);
+	fwrite(&x, sizeof(int), 1, vystupbin);
+	fclose(vystuptxt);
+	fclose(vystupbin);
+
+}
+
+
